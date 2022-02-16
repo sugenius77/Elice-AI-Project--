8411c3aaa426ae5268ab41d01bdc9248550a2b05
ho2yahh <ho2yahh@gmail.com>
2022-02-16 13:55:05 +0900

    Rreact 개발환경 세팅 (라이브러리 추가)

---
+++ front/src/components/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "components/Header";
+import Footer from "components/Footer";
+
+const Layout = ({ children }) => {
+    return (
+        <div>
+            <Header />
+            <div className="content overflow-x-hidden">{children}</div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Layout;
