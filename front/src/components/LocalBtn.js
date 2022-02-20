0e49397014e5fedd08fd5a8ec8eaee53fd440935
ho2yahh <ho2yahh@gmail.com>
2022-02-20 23:36:34 +0900

    [#12] feat:지역 선택 버튼 추가 및 검색 기능 개선

---
+++ front/src/components/LocalBtn.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+
+const LocalBtn = ({ isProperty, setIsProperty }) => {
+    const handlePropertyBtn = (e) => {
+        const { value } = e.target;
+        if (value === "전체") {
+            setIsProperty(["전체"]);
+        } else if (isProperty.length === 5) {
+            setIsProperty(["전체"]);
+        } else if (isProperty.find((e) => e === value)) {
+            setIsProperty(isProperty.filter((e) => e !== value));
+        } else if (isProperty.length > 0) {
+            setIsProperty([...isProperty.filter((e) => e !== "전체"), value]);
+        } else {
+            setIsProperty(["전체"]);
+        }
+    };
+
+    const location = ["전체", "서울", "제주", "부산", "강원", "여수"];
+
+    return (
+        <div className="flex md:justify-around justify-between items-center mb-3">
+            {location.map((local) => {
+                return (
+                    <input
+                        type="button"
+                        value={local}
+                        onClick={handlePropertyBtn}
+                        clicked={isProperty.find((e) => e === { local })}
+                        className={`text-sm border rounded-2xl p-2 cursor-pointer shadow-sm ${
+                            isProperty.includes(local) &&
+                            "bg-point shadow text-white"
+                        } `}
+                    />
+                );
+            })}
+        </div>
+    );
+};
+
+export default LocalBtn;
