39ba0ad044b226c0f3654727fcd7ec5468b9a1a5
me <nr25@naver.com>
2022-02-23 19:09:00 +0900

    [#22]호텔 및 리뷰 관련 dto,model 생성 & 호텔추천 임시 API 생성

---
+++ BE/app/models/model.py
@@ -0,0 +1,33 @@
+from app import db
+from datetime import date, datetime
+
+
+class HotelInfo(db.Model):
+    __tablename__ = "hotel_info"
+    hotel_id = db.Column(db.Integer, primary_key=True,
+                         nullable=False, autoincrement=True)
+    hotel_name = db.Column(db.String, nullable=False)
+    region = db.Column(db.String, nullable=False)
+    hotel_url = db.Column(db.String, nullable=False)
+    hotel_img_url = db.Column(db.String, nullable=False)
+
+    def __init__(self, hotel_name, region, hotel_url, hotel_img_url):
+        self.hotel_name = hotel_name
+        self.region = region
+        self.hotel_url = hotel_url
+        self.hotel_img_url = hotel_img_url
+
+
+class Review(db.Model):
+    __tablename__ = 'review'
+    review_id = db.Column(db.Integer, primary_key=True,
+                          nullable=False, autoincrement=True)
+    contents = db.Column(db.String, nullable=False)
+    hotel_id = db.Column(db.Integer, nullable=False)
+    review_date = db.Column(db.Date, nullable=False)
+    is_positive = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, contents, hotel_id, is_positive):
+        self.contents = contents
+        self.hotel_id = hotel_id
+        self.is_positive = is_positive
--- BE/create-table.sql
+++ BE/create-table.sql
@@ -2,6 +2,7 @@ CREATE TABLE review(
   review_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '리뷰id PK AI',
   contents TEXT COMMENT '리뷰내용',
   hotel_id INTEGER COMMENT '호텔 id',
+  review_date DATE COMMENT '리뷰작성 날짜',
   is_positive INTEGER COMMENT '0: 부정, 1: 긍정'
 ) DEFAULT CHARSET UTF8 COMMENT '리뷰 테이블';
 CREATE TABLE hotel_info(
@@ -10,4 +11,4 @@ CREATE TABLE hotel_info(
   region VARCHAR(255) COMMENT '호텔 지역',
   hotel_url VARCHAR(255) COMMENT '호텔 url',
   hotel_img_url VARCHAR(255) COMMENT '호텔 이미지 url'
-) DEFAULT CHARSET UTF8 COMMENT '부정리뷰 테이블';
+) DEFAULT CHARSET UTF8 COMMENT '호텔정보 테이블';
