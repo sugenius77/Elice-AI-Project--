fd56f3596a92192e3e3cc236f26bda1b9827c66d
ho2yahh <ho2yahh@gmail.com>
2022-02-24 10:30:36 +0900

    [#26] 무한스크롤 구현 중 데이터 슬라이싱 문제

--- front/src/components/Loading.js
+++ front/src/components/Loading.js
@@ -6,3 +6,9 @@ export const Loading = () => (
         <ClimbingBoxLoader color="#fca311" size="15" />
     </div>
 );
+
+export const Loading2 = () => (
+    <div className="flex justify-center items-center ">
+        <ClimbingBoxLoader color="#FF5964" size="15px" />
+    </div>
+);
