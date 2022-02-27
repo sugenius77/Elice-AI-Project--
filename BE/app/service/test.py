415bd26070841ae9a651e0d57888464163b8caa2
HyeonHui Kang <hkang4628@naver.com>
2022-02-27 19:18:13 +0900

    [#18] 최종 CSV 파일로 모델 재학습

--- BE/app/service/test.py
+++ BE/app/service/test.py
@@ -9,7 +9,7 @@ import save_model
 hotel_info_df = pd.read_csv('./dataset/final/hotel_info.csv', encoding='utf-8')
 gangwondo_info = hotel_info_df[hotel_info_df['region'] == '강원도']
 hotel_review_df = pd.read_csv('./dataset/final/hotel_review.csv', encoding='utf-8')
-hotel_review_df.rename(columns={'review' :'contents', 'review_index' : 'review_id', 'hotel_index' :'hotel_id'}, inplace=True)
+hotel_review_df.rename(columns={'review' :'contents'}, inplace=True)
 
 
 # 프론트에서 받은 값으로 변경 필요
@@ -17,7 +17,7 @@ user_input = '화장실이 깨끗하고 객실이 넓은'
 
 #save_model.save_model(hotel_info_df, hotel_review_df)
 start = time.time()
-data = recomend_hotel.get_recomended_hotel(['강원도', '서울', '부산'], user_input)
+data = recomend_hotel.get_recomended_hotel(['강원도'], user_input)
 delta_t = time.time() - start
 print(data)
 print(delta_t)
