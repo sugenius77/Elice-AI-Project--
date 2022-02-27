e2ea9f409da98a8384e7164eb9468b36d9fa8336
sujin <zziinn812@gmail.com>
2022-02-27 19:55:23 +0900

    [#40] 사용자 정보 API 수정 - rest api 문서 내용 수정 - dto 생성

--- BE/app/controller/userApi.py
+++ BE/app/controller/userApi.py
@@ -1,22 +1,40 @@
 from flask import Flask,request,jsonify
 from flask_restx import Resource, Namespace
+from itsdangerous import json
 from app import db
-from ..models.model import UserInfo
+from app.dto.userDto import UserDto
+from app.models.model import UserInfo
 
-user_api = Namespace("user_info")
+user_api = UserDto.api
 
+userParser = user_api.parser()
+userParser.add_argument(
+    'name', type=str, help='이름 (예)김길동', location='json', required=True)
+userParser.add_argument(
+    'email', type=str, help='이메일 (예)example@gmail.com',  location='json',required=True)
 
-@user_api.route('/info')
+@user_api.route("/info", methods=["POST"])
+@user_api.doc(parser=userParser)
+@user_api.response(200, "성공적으로 수행 됨")
+@user_api.response(400, "요청 정보 정확하지 않음")
+@user_api.response(500, "API 서버에 문제가 발생하였음")
 class UserApi(Resource):
+    @user_api.marshal_with(UserDto.user_info_model, envelope="data")
+    @user_api.expect(userParser)
     def post(self):
-        data = request.get_json()
+        args = userParser.parse_args()
+        name = args['name']
+        email = args['email']
         result = UserInfo.query\
-                .filter(UserInfo.name == data['name'], UserInfo.email == data['email'])\
+                .filter(UserInfo.name == name, UserInfo.email == email)\
                 .first()
         if result == None:
-            user = UserInfo(user_id=None,name=data['name'],email=data['email']) 
+            user = UserInfo(user_id=None,name=name,email=email) 
             db.session.add(user)
-            db.session.commit()
-    
-        return jsonify({"status":200,"message":"update success"})
+            db.session.commit() 
+        
+        user_info =  UserInfo.query\
+                .filter(UserInfo.name == name, UserInfo.email == email)\
+                .first()
 
+        return user_info
