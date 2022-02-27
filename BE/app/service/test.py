52dd20c65e252cb99a8ac9af4587d8ad45434114
HyeonHui Kang <hkang4628@naver.com>
2022-02-27 11:43:24 +0900

    [#18] 모델 생성, 추천 호텔 코드 분리, 지역 필터 가능하게 변경

--- BE/app/service/test.py
+++ BE/app/service/test.py
@@ -1,19 +1,23 @@
 import pandas as pd
-
+import time
 import recomend_hotel
+import save_model
 
-
-# 테스트용 코드
+## 테스트용 코드
 
 # csv -> db로 데이터 변경 필요
 hotel_info_df = pd.read_csv('./dataset/final/hotel_info.csv', encoding='utf-8')
-hotel_review_df = pd.read_csv(
-    './dataset/final/hotel_review.csv', encoding='utf-8')
+gangwondo_info = hotel_info_df[hotel_info_df['region'] == '강원도']
+hotel_review_df = pd.read_csv('./dataset/final/hotel_review.csv', encoding='utf-8')
+hotel_review_df.rename(columns={'review' :'contents', 'review_index' : 'review_id', 'hotel_index' :'hotel_id'}, inplace=True)
+
 
 # 프론트에서 받은 값으로 변경 필요
-user_input = '직원이 친절하고 화장실이 깨끗'
+user_input = '화장실이 깨끗하고 객실이 넓은'
 
-recomended_hotel_data = recomend_hotel.get_recomended_hotel(
-    hotel_info_df, hotel_review_df, user_input)
-
-print(recomended_hotel_data)
+#save_model.save_model(hotel_info_df, hotel_review_df)
+start = time.time()
+data = recomend_hotel.get_recomended_hotel(['강원도', '서울', '부산'], user_input)
+delta_t = time.time() - start
+print(data)
+print(delta_t)
