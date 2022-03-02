8b1ca6c2b6486b2bfb51c6047125847e0160c9e9
me <nr25@naver.com>
2022-03-02 13:41:35 +0900

    router camel -> _로 변경

--- BE/app/controller/hotelApi.py
+++ BE/app/controller/hotelApi.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 from app.dto.hotelDto import HotelDto
 from app import db
-from app.models.model import HotelInfo, Review
+from app.models.model import Hotel, Review
 from app.service import recomend_hotel
 
 hotel_api = HotelDto.api
@@ -18,12 +18,12 @@ hotelParser.add_argument(
     'search', type=str, help='검색텍스트 (예)바다가 보이고 침대가 편안한 호텔 찾아줘', location='args', required=True)
 
 
-@hotel_api.route("/recommendHotelList", methods=["GET"])
+@hotel_api.route("/recommend-hotel-list", methods=["GET"])
 @hotel_api.doc(parser=hotelParser)
 @hotel_api.response(200, "성공적으로 수행 됨")
 @hotel_api.response(400, "요청 정보 정확하지 않음")
 @hotel_api.response(500, "API 서버에 문제가 발생하였음")
-class HelloApi(Resource):
+class RecommendHotelApi(Resource):
     @hotel_api.marshal_with(HotelDto.recoHotel_list_model, envelope="data")
     @hotel_api.expect(hotelParser)
     def get(self):
@@ -38,15 +38,16 @@ class HelloApi(Resource):
         print(
             f'region={region}, search={search}')
 
-        #hotel_info_df = pd.DataFrame(db.session.query(HotelInfo).filter(HotelInfo.region == region), columns=['hotel_id', 'hotel_name', 'region', 'hotel_url', 'hotel_img_url'])
+        #hotel_info_df = pd.DataFrame(db.session.query(Hotel).filter(Hotel.region == region), columns=['hotel_id', 'hotel_name', 'region', 'hotel_url', 'hotel_img_url'])
         #hotel_review_df = pd.DataFrame(db.session.query(Review), columns=['review_id', 'contents', 'hotel_id', 'review_date', 'is_positive'])
 
-        # hotel_info_df = pd.read_sql(db.session.query(HotelInfo).filter(
-        #     HotelInfo.region == region).statement, db.session.bind)
+        # hotel_info_df = pd.read_sql(db.session.query(Hotel).filter(
+        #     Hotel.region == region).statement, db.session.bind)
 
         # hotel_review_df = pd.read_sql(
-        #     db.session.query(Review).join(HotelInfo, Review.hotel_id == HotelInfo.hotel_id).filter(HotelInfo.region == region).statement, db.session.bind)
-
+        #     db.session.query(Review).join(Hotel, Review.hotel_id == Hotel.hotel_id).filter(Hotel.region == region).statement, db.session.bind)
+        # TODO 지역 코드화(프론트-백엔트-AI 통일)
+        # TODO DB 데이터로 변경?
         if region == "전체":
             region_list = ['서울', '부산', '제주', '강원', '여수']
         else:
@@ -61,10 +62,36 @@ class HelloApi(Resource):
         return hotel_list
 
 
+hotelInfoParser = hotel_api.parser()
+hotelInfoParser.add_argument(
+    'hotel_id', type=int, help='호텔 id', location='args', required=True)
+
+
+@hotel_api.route("/hotel-info", methods=["GET"])
+@hotel_api.doc(parser=hotelInfoParser)
+@hotel_api.response(200, "성공적으로 수행 됨")
+@hotel_api.response(400, "요청 정보 정확하지 않음")
+@hotel_api.response(500, "API 서버에 문제가 발생하였음")
+class HotelInfoApi(Resource):
+    @hotel_api.marshal_with(HotelDto.hotel_model, envelope="data")
+    @hotel_api.expect(hotelInfoParser)
+    def get(self):
+        '''호텔 정보 데이터
+        '''
+        args = hotelInfoParser.parse_args()
+        hotel_id = args['hotel_id']
+        if hotel_id == None:
+            abort(400, msg='요청 정보 정확하지 않음.')
+
+        print(f'hotel_id={hotel_id}')
+
+        return db.session.query(Hotel).filter(Hotel.hotel_id == hotel_id).first()
+
+
 def hotelval(x):
 
     hotels = {}
-    for u in db.session.query(HotelInfo).filter(HotelInfo.hotel_id == x['hotel_id']).all():
+    for u in db.session.query(Hotel).filter(Hotel.hotel_id == x['hotel_id']).all():
         hotels = u.__dict__
 
     reviews = list(map(reviewval, x['review_id']))
