9246a91eb69efc84f4abc8530697783e4c3a64f1
sujin <zziinn812@gmail.com>
2022-03-02 18:13:35 +0900

    [#30] 찜기능 Api
    - 찜 목록 보기, 찜 목록 제거, 찜 목록 추가 Api
    - 중간 발표 피드백: user_info -> user로 변경

--- BE/create-table.sql
+++ BE/create-table.sql
@@ -13,8 +13,15 @@ CREATE TABLE hotel_info(
   hotel_img_url VARCHAR(2048) COMMENT '호텔 이미지 url'
 ) DEFAULT CHARSET UTF8 COMMENT '호텔정보 테이블';
 
-CREATE TABLE user_info(
-  user_id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '고객 index PK',
-  name text DEFAULT NULL COMMENT '고객 이름',
-  email text DEFAULT NULL COMMENT '고객 이메일',
-) DEFAULT CHARSET UTF8 COMMENT '고객 정보 테이블';
+CREATE TABLE `user` (
+  `user_id` varchar(255) NOT NULL PRIMARY KEY COMMENT '사용자id PK',
+  `name` varchar(255) DEFAULT NULL COMMENT '사용자 이름',
+  `email` varchar(255) DEFAULT NULL COMMENT '사용자 이메일',
+) DEFAULT CHARSET UTF8 COMMENT='사용자 정보 테이블'
+
+CREATE TABLE `wish_list` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `user_id` varchar(255) NOT NULL COMMENT '사용자id',
+  `hotel_id` int(11) NOT NULL COMMENT '호텔id',
+) DEFAULT CHARSET UTF8 COMMENT='찜 목록 테이블'
+
