08c01eb992b9dc52715ae1d48fad595aa7788c11
ho2yahh <ho2yahh@gmail.com>
2022-03-03 23:42:18 +0900

    front polish

---
+++ front/src/action/ProgressBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Progressbar = ({ value }) => {
+    return (
+        //  {/* // Progress bar FIX 유사도 값 넣어야함 */}
+        <div className="mt-2">
+            <div className="w-full bg-gray-200 rounded-full">
+                <div
+                    className="bg-point text-xs font-medium text-gray-500 text-center p-0.5 leading-none rounded-full"
+                    style={{ width: "45%" }}
+                >
+                    Similarity : 45%
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Progressbar;
