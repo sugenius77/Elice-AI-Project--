9b48c920a5bc4c31df8cd2cf17d8997012f3031b
sujin <zziinn812@gmail.com>
2022-03-03 23:00:49 +0900

    [#52] 찜 목록 슬라이드 구현
    - @headlessui/react, @heroicons/react/outline 모듈 추가
    - WishList.js 컴포넌트 추가

--- front/src/App.js
+++ front/src/App.js
@@ -2,69 +2,71 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "pages/Home";
 import Header from "components/Header";
-import Likes from "pages/Likes";
 import { useRecoilState } from "recoil";
 import { userInfoState } from "state/atom";
+import { wishListState } from "state/atom";
 import axios from "axios";
 import Detail from "pages/Detail";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect } from "react";
 
 function App() {
-    const access_token =
-        window.location.hash.split("&")[0].split("=")[1] ||
-        localStorage.access_token;
-    const [userInfo, setUserInfo] = useRecoilState(userInfoState);
-    const handleUserInfoChange = useCallback((info) => {
-        setUserInfo(info);
-    }, []);
+  const access_token =
+    window.location.hash.split("&")[0].split("=")[1] ||
+    localStorage.access_token;
+  const [userInfo, setUserInfo] = useRecoilState(userInfoState);
+  const [wishList, setWishList] = useRecoilState(wishListState);
+  const handleUserInfoChange = useCallback((info) => {
+    setUserInfo(info);
+  }, []);
 
-    const asyncGetUserInfo = async (token) => {
-        try {
-            const { data: res } = await axios.get(
-                `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${token}`
-            );
-            if (Object.keys(res).length) {
-                handleUserInfoChange(res);
-                window.history.replaceState({}, null, "/");
-                await axios.post(
-                    //   `${process.env.REACT_APP_API}/user/info`
-                    "http://0.0.0.0:1234/user/info",
-                    { id: res.id, name: res.name, email: res.email },
-                    {
-                        headers: {
-                            "Content-Type": `application/json`,
-                        },
-                    }
-                );
-            }
-        } catch (e) {
-            console.error(e);
-        }
-    };
-    useEffect(() => {
-        if (access_token) {
-            localStorage.setItem("access_token", access_token);
-            asyncGetUserInfo(access_token);
-        }
-    }, [access_token]);
+  const asyncGetUserInfo = async (token) => {
+    try {
+      const { data: res } = await axios.get(
+        `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${token}`
+      );
+      if (Object.keys(res).length) {
+        handleUserInfoChange(res);
+        window.history.replaceState({}, null, "/");
+        await axios.post(
+          //   `${process.env.REACT_APP_API}/user/info`
+          "http://0.0.0.0:1234/user/info",
+          { id: res.id, name: res.name, email: res.email },
+          {
+            headers: {
+              "Content-Type": `application/json`,
+            },
+          }
+        );
+        const response = await axios.get(
+          `http://0.0.0.0:1234/wish-list/${res.id}`
+        );
+        setWishList(response.data.data);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-    return (
-        <Router>
-            <Header userInfo={userInfo} />
+  useEffect(() => {
+    if (access_token) {
+      localStorage.setItem("access_token", access_token);
+      asyncGetUserInfo(access_token);
+    }
+  }, [access_token]);
 
-            <Switch>
-                <Route
-                    path="/"
-                    exact
-                    render={() => (
-                        <Home handleUserInfoChange={handleUserInfoChange} />
-                    )}
-                />
-                <Route path="/likes" render={() => <Likes />} />
-                <Route path="/hotel/:_id" component={Detail} />
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <Header userInfo={userInfo} />
+      <Switch>
+        <Route
+          path="/"
+          exact
+          render={() => <Home handleUserInfoChange={handleUserInfoChange} />}
+        />
+        <Route path="/hotel/:_id" component={Detail} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
