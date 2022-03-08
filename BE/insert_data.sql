c80a927a765eada0e297a168af7ffa86833c2272
me <nr25@naver.com>
2022-03-08 23:33:55 +0900

    [#62]상세페이지 워드클라우드, 주소, 유사도 반환

--- BE/insert_data.sql
+++ BE/insert_data.sql
@@ -1,9 +1,12 @@
-LOAD DATA INFILE 'hotel_info.csv' INTO TABLE hotel FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES(
+LOAD DATA INFILE 'hotel_info.csv' INTO TABLE hotel FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\r' IGNORE 1 LINES(
   region,
   hotel_id,
   hotel_name,
+  address,
   hotel_url,
-  hotel_img_url
+  hotel_img_url,
+  positive_keywords,
+  negative_keywords
 );
 LOAD DATA INFILE 'hotel_review.csv' INTO TABLE review FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (
   review_id,
